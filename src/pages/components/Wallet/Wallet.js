30c673f9403b05bf963e48925dd37f226a113104
x-dev-private <gabrielcostaribeiro666@gmail.com>
2024-07-12 12:12:34 -0300

    wallet update

--- src/pages/components/Wallet/Wallet.js
+++ src/pages/components/Wallet/Wallet.js
@@ -5,10 +5,7 @@ import Web3 from 'web3';
 const Wallet = () => {
     const [web3, setWeb3] = useState(null);
     const [account, setAccount] = useState(null);
-    const [balance, setBalance] = useState(null);
-    const [toAddress, setToAddress] = useState('');
-    const [amount, setAmount] = useState('');
-    const [isConnected, setIsConnected] = useState(false); // Add this state
+    const [isConnected, setIsConnected] = useState(false);
 
     useEffect(() => {
         if (window.ethereum) {
@@ -28,8 +25,6 @@ const Wallet = () => {
                 await window.ethereum.request({ method: 'eth_requestAccounts' });
                 const accounts = await web3.eth.getAccounts();
                 setAccount(accounts[0]);
-                const balance = await web3.eth.getBalance(accounts[0]);
-                setBalance(web3.utils.fromWei(balance, 'ether'));
                 setIsConnected(true); // Set isConnected to true when connected
             } catch (error) {
                 console.error('Erro ao conectar à carteira:', error);
@@ -37,51 +32,14 @@ const Wallet = () => {
         }
     };
 
-    const sendTransaction = async () => {
-        if (web3 && account) {
-            try {
-                const value = web3.utils.toWei(amount, 'ether');
-                await web3.eth.sendTransaction({
-                    from: account,
-                    to: toAddress,
-                    value: value
-                });
-                alert('Transação enviada!');
-            } catch (error) {
-                console.error('Erro ao enviar transação:', error);
-            }
-        }
-    };
-
     return (
         <div className="wallet-container">
-            <h1>React Wallet</h1>
-            <button onClick={connectWallet}>Conectar Wallet</button>
-        <div>
-            {!isConnected && ( // Conditionally render the button
-                <button onClick={connectWallet}>Conectar Wallet</button>
-            )}
-            {account && (
+            {isConnected? (
                 <div>
-                    <p><strong>Conta:</strong> {account}</p>
-                    <p><strong>Saldo:</strong> {balance} ETH</p>
-                    <div>
-                        <h2>Enviar Transação</h2>
-                        <input
-                            type="text"
-                            placeholder="Endereço do Destinatário"
-                            value={toAddress}
-                            onChange={(e) => setToAddress(e.target.value)}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Valor em ETH"
-                            value={amount}
-                            onChange={(e) => setAmount(e.target.value)}
-                        />
-                        <button onClick={sendTransaction}>Enviar</button>
-                    </div>
+                    <p><strong>Conta:</strong> {account.slice(0, 7)}...</p>
                 </div>
+            ) : (
+                <button onClick={connectWallet}>Conectar Wallet</button>
             )}
         </div>
     );
