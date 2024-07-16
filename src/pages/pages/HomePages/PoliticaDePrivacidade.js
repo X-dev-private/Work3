2e52257cebb84a6908e0f7a1a754e0a18b8a4b76
mestrecamus <feliperua2002@hotmail.com>
2024-07-16 10:18:29 -0300

    AddPoliticaDePrivacidade

---
+++ src/pages/pages/HomePages/PoliticaDePrivacidade.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <main>
+        <div class="container">
+        <h1>Política de Privacidade</h1>
+        <p>Bem-vindo ao site da Work 3. A sua privacidade é muito importante para nós. Esta política de privacidade descreve como coletamos, usamos, protegemos e compartilhamos suas informações pessoais quando você usa nosso site.</p>
+
+        <h2>1. Informações que Coletamos</h2>
+        <p>Coletamos informações de diferentes maneiras, incluindo:</p>
+        <ul>
+            <li><strong>Informações que você nos fornece:</strong> Quando você se registra em nosso site, assina nosso boletim informativo, preenche um formulário ou entra em contato conosco, podemos coletar informações pessoais, como nome, e-mail, endereço e número de telefone.</li>
+            <li><strong>Informações que coletamos automaticamente:</strong> Quando você visita nosso site, podemos coletar automaticamente certas informações sobre seu dispositivo, incluindo seu endereço IP, tipo de navegador, provedor de serviços de Internet (ISP), páginas de referência/saída, e carimbos de data/hora.</li>
+            <li><strong>Cookies e tecnologias semelhantes:</strong> Utilizamos cookies e tecnologias semelhantes para coletar informações sobre como você interage com nosso site, para melhorar sua experiência de navegação e personalizar o conteúdo que oferecemos.</li>
+        </ul>
+
+        <h2>2. Como Usamos Suas Informações</h2>
+        <p>As informações que coletamos são usadas de várias maneiras, incluindo:</p>
+        <ul>
+            <li>Para fornecer, operar e manter nosso site;</li>
+            <li>Para melhorar, personalizar e expandir nosso site;</li>
+            <li>Para entender e analisar como você usa nosso site;</li>
+            <li>Para desenvolver novos produtos, serviços, recursos e funcionalidades;</li>
+            <li>Para comunicar-se com você, diretamente ou através de um de nossos parceiros, inclusive para atendimento ao cliente, para fornecer atualizações e outras informações relacionadas ao site, e para fins de marketing e promocionais;</li>
+            <li>Para processar suas transações e gerenciar seu registro e conta;</li>
+            <li>Para encontrar e prevenir fraudes;</li>
+        </ul>
+
+        <h2>3. Compartilhamento de Informações</h2>
+        <p>Não vendemos, comercializamos ou transferimos suas informações pessoais para terceiros, exceto nas seguintes circunstâncias:</p>
+        <ul>
+            <li>Com seu consentimento explícito;</li>
+            <li>Para nossos parceiros confiáveis que nos auxiliam na operação do nosso site, na condução dos nossos negócios, ou no atendimento a você, desde que essas partes concordem em manter essas informações confidenciais;</li>
+            <li>Quando acreditamos que a divulgação é apropriada para cumprir a lei, fazer cumprir nossas políticas de site, ou proteger os nossos direitos, propriedade ou segurança de outros;</li>
+        </ul>
+
+        <h2>4. Segurança de Dados</h2>
+        <p>Implementamos uma variedade de medidas de segurança para manter a segurança de suas informações pessoais. Seus dados pessoais são armazenados em redes seguras e só são acessíveis por um número limitado de pessoas que têm direitos especiais de acesso a esses sistemas, e são obrigadas a manter a confidencialidade das informações.</p>
+
+        <h2>5. Seus Direitos</h2>
+        <p>Você tem o direito de acessar, corrigir, atualizar ou excluir suas informações pessoais. Para fazer isso, entre em contato conosco através das informações de contato fornecidas abaixo. Responderemos à sua solicitação dentro de um prazo razoável.</p>
+
+        <h2>6. Alterações nesta Política de Privacidade</h2>
+        <p>Podemos atualizar nossa política de privacidade de tempos em tempos. Publicaremos quaisquer alterações nesta página, portanto, revise periodicamente. O uso contínuo do site após a publicação de quaisquer mudanças constitui aceitação dessas mudanças.</p>
+    </div>
+        </main>
+    );
+};
+
+export default About;
